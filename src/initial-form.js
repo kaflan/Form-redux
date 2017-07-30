36e418fa8dfa0fde6c1f7b54a80b67d92368f7d8
kaflan <kaf_90@mail.ru>
2017-07-30 21:17:55 +0300

    add fix dreop down items

--- src/initial-form.js
+++ src/initial-form.js
@@ -12,11 +12,11 @@ const data = {
 
 const DropDownMenu = (props) => {
   return (
-    <div className="dropdown">
-      dropdown
+    <div className="dropdown"  role="group">
+     
     <button className="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
-        Dropdown
-    <span className="caret"></span>
+        <span className="beforecaret">Dropdown</span>
+    <span className="caret" ></span>
       </button>
       <ul className="dropdown-menu" aria-labelledby="dropdownMenu1">
         <li><a>Action</a></li>
